fc361712da7258a5b07b2caa290011353a868867
MonsieurWai <monsieurwai@gmail.com>
2020-02-10 14:02:05 +0100

    first commit

---
+++ dist/js/main/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var sayWord = function sayWord(word) {
+  console.log(word);
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  sayWord('wow !');
+});
+
+//# sourceMappingURL=main.js.map
---
+++ dist/js/specific/contact.js
@@ -0,0 +1,6 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("ce script ne se lance que sur la page contact");
+});
+//# sourceMappingURL=contact.js.map
